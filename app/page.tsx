1d6e5502d82bf7978c478ae557c5001e4c79c7e9
jellablue <143723671+jellablue@users.noreply.github.com>
2026-04-23 23:27:37 +0800

    update main container and components

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,26 @@ export default async function Home() {
 
   return (
     <main>
-      {/* Hero */}
-      <section id="hero"     className="min-h-screen flex items-center justify-center px-8">
+        {/* Hero */}
+      <section id="hero" className="min-h-screen flex items-center justify-center px-8">
         <Hero/>
       </section>
 
       {/* About */}
-      <section id="about"    className="min-h-screen flex items-center px-8">
+      <section id="about" className="min-h-screen flex items-center justify-center px-8">
         <About/>
       </section>
 
       {/* Projects */}
-       <section id="projects" className="min-h-screen px-8 py-20">
+       <section id="projects" className="min-h-screen flex items-center justify-center px-8">
         <Projects/>
       </section>
       
       {/* Contact */}
-      <section id="contact"  className="min-h-screen flex items-center px-8">
+      <section id="contact"  className="min-h-screen flex items-center justify-center px-8">
         <Contact/>
       </section>
-
+     
     </main>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ['192.168.1.5', 'localhost'],
+  devIndicators: false,
 };
 
 module.exports = {
